fbe8b07c240ecb89e54bb46249ce9e5a305b4908
Thiaguo <thiagotadeunicolaumariano5@gmail.com>
2022-06-24 12:55:08 -0300

    segundo commit

---
+++ Aprend/index.js
@@ -0,0 +1,26 @@
+const resposta = document.querySelector("#resposta");
+const inputPegunta = document.querySelector("#inputPergunta");
+const button = document.querySelector("#buttonPerguntar");
+const respostas = ["Sim","Não","Talvez"];
+
+
+
+function mostrarResposta(){
+
+    if(inputPergunta.value == ""){
+        alert("Faça sua Pergunta, por favor")
+        return
+    }
+    button.setAttribute("disabled", true);
+    //Desabila o Botão
+    const pergunta = "<div>" + inputPergunta.value + "</div>";
+    const totalResposta = respostas.length;
+    const numeroAleatorio = Math.floor(Math.random() * totalResposta);
+    resposta.innerHTML = pergunta + respostas[numeroAleatorio];
+        resposta.style.opacity = 1;
+    setTimeout(function(){
+        resposta.style.opacity = 0;
+        button.removeAttribute("disabled");
+        //Abilita Novamente o Botão
+    }, 3000);
+}
